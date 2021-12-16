16a8a7f7a427c9e5ed35b35ee04972da35a4e52f
Nitsua365 <austinblanchard@hotmail.com>
2021-12-16 10:21:53 -0600

    added mongoDB client

--- Backend/endpoints.js
+++ Backend/endpoints.js
@@ -2,6 +2,22 @@ const express = require('express');
 const chalk = require('chalk');
 const app = express();
 const port = 8080;
+const apikeys = require('./api-keys')
+const { MongoClient } = require('mongodb')
+const uri = "mongodb+srv://" + 
+            apikeys.mongoKeys.mongoUser + 
+            ":<" + apikeys.mongoKeys.mongoPass + ">@" + 
+            apikeys.mongoKeys.mongoPass + "/" + 
+            apikeys.mongoKeys.mongodbName + 
+            "?retryWrites=true&w=majority";
+
+const mongoClient = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoClient.connect(err => {
+  const collection = mongoClient.db(apikeys.mongoKeys.mongodbName).collection("devices");
+  console.log(collection.collectionName);
+  mongoClient.close();
+});
 
 app.use(express.json());
 
@@ -9,7 +25,6 @@ app.post('/home', (req, res) => {
   console.log(req.body);
   res.send(req.body);
 });
-
 
 // listen on port 8080
 app.listen(port, () => {
