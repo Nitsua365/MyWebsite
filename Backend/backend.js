99a512dd6b81b9e6d0bb68d5700e23a7574a4f84
Nitsua365 <austinblanchard@hotmail.com>
2021-12-15 19:12:46 -0600

    setup the backend endpoints

--- Backend/backend.js
+++ Backend/backend.js
@@ -1,2 +1,17 @@
-const express = require('express')
-const mongo = require('mongodb').MongoClient
+const express = require('express');
+const chalk = require('chalk');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.post('/home', (req, res) => {
+  console.log(req.body);
+  res.send(req.body);
+});
+
+
+// listen on port 8080
+app.listen(port, () => {
+  console.log(chalk.hex("#006eff")("Running on: http://localhost:" + port));
+})
